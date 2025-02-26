a0d4ffa8d4a3e8f26f5336c82600da17202a38ad
Anthony Le <87265509+AnthonyLe93@users.noreply.github.com>
2025-02-26 10:49:16 +0000

    hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    using namespace std;
+    cout << "Hi There!" << endl;
+    cout << endl << endl;
+    return 0;
+}
